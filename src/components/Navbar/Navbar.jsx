f6c994704869727ef28e101ec71940b3b92164b8
Agustin Fraile <agustinfraile98@gmail.com>
2024-11-11 23:50:37 -0300

    agregando estilos a la navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,29 @@
-// Navbar.jsx
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
-import { Link } from 'react-router-dom';
-
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <header className={styles.navbar}>
             <Link to="/" className={styles.logoLink}>
                 <h1 className={styles.logo}>DAHACHE</h1>
             </Link>
-            <nav className={styles.navLinks}>
-                <Link to="/coleccionhombre">Colección Hombre</Link>
-                <Link to="/coleccionmujer">Colección Mujer</Link>
+
+            {/* Icono de menú para dispositivos móviles */}
+            <div className={styles.menuIcon} onClick={toggleMenu}>
+                ☰
+            </div>
+
+            {/* Links de navegación */}
+            <nav className={`${styles.navLinks} ${isMenuOpen ? styles.showMenu : ''}`}>
+                <Link to="/coleccionhombre" onClick={() => setIsMenuOpen(false)}>Colección Hombre</Link>
+                <Link to="/coleccionmujer" onClick={() => setIsMenuOpen(false)}>Colección Mujer</Link>
             </nav>
         </header>
     );
