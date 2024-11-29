887148926b9e4c856e96e1e5c374a54ac593a518
Agustin Fraile <agustinfraile98@gmail.com>
2024-11-29 15:17:01 -0300

    modificando el hero del bf

--- src/components/BlackFridayHero/BlackFridayHero.jsx
+++ src/components/BlackFridayHero/BlackFridayHero.jsx
@@ -2,41 +2,7 @@ import { useEffect, useState } from 'react';
 import styles from './BlackFridayHero.module.css';
 
 const BlackFridayHero = () => {
-    const [timeLeft, setTimeLeft] = useState({
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-    });
 
-    // Función para calcular el tiempo restante
-    const calculateTimeLeft = () => {
-        const blackFridayDate = new Date('2024-11-29T00:00:00'); // Fecha del Black Friday
-        const now = new Date();
-        const difference = blackFridayDate - now;
-
-        if (difference > 0) {
-            setTimeLeft({
-                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-                minutes: Math.floor((difference / (1000 * 60)) % 60),
-                seconds: Math.floor((difference / 1000) % 60),
-            });
-        } else {
-            setTimeLeft({
-                days: 0,
-                hours: 0,
-                minutes: 0,
-                seconds: 0,
-            });
-        }
-    };
-
-    // Actualizar el contador cada segundo
-    useEffect(() => {
-        const timer = setInterval(calculateTimeLeft, 1000);
-        return () => clearInterval(timer); // Limpiar el intervalo al desmontar el componente
-    }, []);
 
     return (
         <section className={styles.heroSection}>
@@ -44,7 +10,7 @@ const BlackFridayHero = () => {
                 <div className={styles.content}>
                     <h1 className={`${styles.title} animate-pulse`}>LLEGÓ EL BLACK FRIDAY</h1>
                     <p className={styles.subtitle}>
-                        Hasta <span className={styles.highlight}>70% OFF</span> en productos seleccionados
+                        Descuentos en <span className={styles.highlight}>todos los productos</span>
                     </p>
                 </div>
             </div>
